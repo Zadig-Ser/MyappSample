afcf52dd12e0e79c82f6ca8e58b09f4b82cf8534
Zadig-Ser <58415205+Zadig-Ser@users.noreply.github.com>
2023-04-24 01:01:18 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('Hello!');
+
+<div className='container'>
+<div class Name='header'>
+<div className='tabcontainer'>
+<div className='dashBoard>Dashboard</div>
+<div className='client'>Client</div>
+<div className='warehouse'>Warehouse
+<div className='warehouseCheckIn>Check In</div>
+<div className='warehouseCheckOut>Check Out</div>
+</div>
+<div className='documents'>Document</div>
+</div>
+</div>
+</div>
